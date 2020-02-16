a60c6a516d4cf742b7e09fa5b262efee0ef086e3
Endymion Cheung <endymion.cheung@gmail.com>
2020-02-16 16:41:16 -0500

    Implemented gradual transition of color for lidar points
    Change the color of the Lidar points such that X=0.0m corresponds to red while X=20.0m is shown as green with a gradual transition in between

--- Exercise/lidar_to_camera/src/show_lidar_top_view.cpp
+++ Exercise/lidar_to_camera/src/show_lidar_top_view.cpp
@@ -13,40 +13,61 @@ void showLidarTopview()
     std::vector<LidarPoint> lidarPoints;
     readLidarPts("../dat/C51_LidarPts_0000.dat", lidarPoints);
 
-    cv::Size worldSize(10.0, 20.0); // width and height of sensor field in m
-    cv::Size imageSize(1000, 2000); // corresponding top view image in pixel
+    // Width and height (driving direction) of sensor field in [m]
+    cv::Size worldSize(10.0, 20.0);
+    // Corresponding width and height of the top view image in [pixel]
+    cv::Size imageSize(1000, 2000);
 
-    // create topview image
-    cv::Mat topviewImg(imageSize, CV_8UC3, cv::Scalar(0, 0, 0));
+    // Create topview image with black color by default
+    cv::Mat topviewImg(imageSize, CV_8UC3, cv::Scalar(0, 0, 0)); 
 
-    // plot Lidar points into image
+    // Plot Lidar points into image
     for (auto it = lidarPoints.begin(); it != lidarPoints.end(); ++it)
     {
-        float xw = (*it).x; // world position in m with x facing forward from sensor
-        float yw = (*it).y; // world position in m with y facing left from sensor
+        // Sensor position in [meters]
+        float x_world = it->x; // world position in m with x facing forward from sensor
+        float y_world = it->y; // world position in m with y facing left from sensor
 
-        int y = (-xw * imageSize.height / worldSize.height) + imageSize.height;
-        int x = (-yw * imageSize.height / worldSize.height) + imageSize.width / 2;
-
-        cv::circle(topviewImg, cv::Point(x, y), 5, cv::Scalar(0, 0, 255), -1);
+        // Image in the top view coordinate system
+        int y = (-x_world * imageSize.height / worldSize.height) + imageSize.height;
+        int x = (-y_world * imageSize.height / worldSize.height) + imageSize.width / 2;
         
         // TODO: 
         // 1. Change the color of the Lidar points such that 
-        // X=0.0m corresponds to red while X=20.0m is shown as green.
+        // x_world = 0.0m corresponds to red while x_world = 20.0m is shown as green
+
+        // Only consider the Lidar points above ground
+        float z_world = it->z; // height above the ground
+        double minZ = -1.40; // [m]
+        if (z_world > minZ)
+        {
+            // Current Lidar point in x-direction [pixels]
+            float val = it->x;
+            // Max Lidar detection range in x-direction [pixels]
+            float maxVal = worldSize.height; 
+            // Scale the red to green based on the max Lidar range
+            int red   = min(255, (int)(255 * abs((maxVal - val/ maxVal))));
+            int green = min(255, (int)(255 * (1 - abs((maxVal - val)/ maxVal))));
+            // Overlay the Lidar points on blank image
+            cv::circle(topviewImg, cv::Point(x, y), 5, cv::Scalar(0, green, red), -1);
+        }
+
         // 2. Remove all Lidar points on the road surface while preserving 
         // measurements on the obstacles in the scene.
     }
 
-    // plot distance markers
-    float lineSpacing = 2.0; // gap between distance markers
+    // Plot distance markers
+    float lineSpacing = 2.0; // gap between distance markers in [m]
     int nMarkers = floor(worldSize.height / lineSpacing);
+    // Draw horziontal lines for every line space
     for (size_t i = 0; i < nMarkers; ++i)
     {
+        // Paint a blue line that runs horizontally across the top of the image
         int y = (-(i * lineSpacing) * imageSize.height / worldSize.height) + imageSize.height;
         cv::line(topviewImg, cv::Point(0, y), cv::Point(imageSize.width, y), cv::Scalar(255, 0, 0));
     }
 
-    // display image
+    // Display image
     string windowName = "Top-View Perspective of LiDAR data";
     cv::namedWindow(windowName, 2);
     cv::imshow(windowName, topviewImg);
